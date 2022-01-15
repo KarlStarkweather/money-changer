eb8a8f9f475d1f788b8b10ae0fef48fa8cc64cc8
KarlStarkweather <stark13@usa.net>
2022-01-14 16:16:47 -0800

    change statis method to return just one conversion rate

--- src/money-changer.js
+++ src/money-changer.js
@@ -1,11 +1,11 @@
 export default class Exchange {
-  static getRates() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+  static getRates(currency) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}`)
       .then(function(response) {
         if(!response.ok) {
           throw Error(response.statusText);
         } 
-        return response.json();
+        return response.json().conversion_rate();
       })
       .catch(function(error) {
         return error;
