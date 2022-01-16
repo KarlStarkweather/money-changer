3e0e8cbfd499c0e869ea56e24f51cdd660a726c6
KarlStarkweather <stark13@usa.net>
2022-01-15 18:43:42 -0800

    cleanup of obsolete or debugging lines

--- src/index.js
+++ src/index.js
@@ -2,26 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/css/styles.css';
-// import '../src/money-changer'
 import Exchange from '../src/money-changer';
 
-// function displayResult(response) {
-//   const amount = $('#amount').val();
-//   console.log(response);
-//   if (response.conversion_rate) {
-//     const rate = response.conversion_rate;
-//     $('#result').html = $(`<p>The converted amount is: ${(rate * amount)}</p>`);
-//   } else {
-//     $('.showErrors').text(`There was an error: ${response.message}`);
-//   }
-// }
 
 $(document).ready(function() {
   $('#convert').click(function() {
     event.preventDefault();
     const currency = $('#currency').val();
     const amount = $('#amount').val();
-    console.log(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}`);
     if($('#amount').val()>0) {
       Exchange.getRate(currency)  
         .then(function(response) {
